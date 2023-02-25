1341f2ee971f81a9f16230c1b6ddf75db197f7e4
Lelik Jan <21alex7777@gmail.com>
2023-02-25 22:26:41 +0300

    Updates

--- src/js/helper.js
+++
@@ -1,8 +0,0 @@
-import {ENG} from "./config";
-
-export const setLocalStorage = (key, value) => {
-    localStorage.setItem(key, value);
-}
-export const getLocalStorage = (value) => localStorage.getItem(value);
-
-export const getEl = (selector) => document.querySelector(selector);
